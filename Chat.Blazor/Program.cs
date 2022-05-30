4c5e44c5b0bceb25138ddce825e27c428b11f10f
Ali <alifarahnak>
2022-05-30 07:02:55 +0000

    m

--- Chat.Blazor/Program.cs
+++ Chat.Blazor/Program.cs
@@ -1,4 +1,5 @@
 using Chat.Blazor;
+using Chat.Core;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -6,6 +7,8 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<ChatViewModel>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
---
+++ Chat.Core/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+
+namespace Chat.Core;
+
+public abstract class BaseViewModel : ObservableObject, IAsyncDisposable
+{
+    public async ValueTask DisposeAsync()
+    {
+    }
+}
