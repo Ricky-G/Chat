efc963e59d2d3dc168eb69496da27f10881ef48d
Ali <alifarahnak>
2022-05-31 18:20:15 +0000

    m

--- Chat.Blazor/Program.cs
+++ Chat.Blazor/Program.cs
@@ -1,5 +1,4 @@
 using Chat.Blazor;
-using Chat.Core;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -7,8 +6,6 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddSingleton<ChatViewModel>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
