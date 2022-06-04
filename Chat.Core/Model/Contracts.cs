e4e85fb25d933904cbba50e502c6666e7bb8470a
Ali <alifarahnak>
2022-06-04 16:54:53 +0000

    m

--- Chat.Core/Model/Contracts.cs
+++ Chat.Core/Model/Contracts.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Chat.Core.Model;
 
-public record class Search(string Title, string Year, string imdbID, string Type, string Poster);
-public record class MovieSearch(List<Search> Search, string totalResults, string Response);
-public record Message(string Name, string Body);
+public record struct Search(string Title, string Year, string imdbID, string Type, string Poster);
+public record struct MovieSearch(List<Search> Search, string totalResults, string Response);
+public record struct Message(string Name, string Body);
 public record struct TelemetrySettings(string AppInsights, string QuickPulse);
