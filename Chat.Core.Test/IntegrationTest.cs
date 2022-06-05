0271fc96c8d748eee0f164cef46e370bb72654b6
Ali <alifarahnak>
2022-06-05 17:17:42 +0000

    m

--- Chat.Core.Test/IntegrationTest.cs
+++ Chat.Core.Test/IntegrationTest.cs
@@ -4,25 +4,8 @@ namespace Chat.Core.Test;
 public class IntegrationTest
 {
     [Fact]
-    public void FruitServiceTest()
-    {
-        FruitService fruitService = new();
-        List<Fruit> fruits = new();
 
-        // Add 3 fruits
-        fruits.Add(fruitService.GetFruit());
-        fruits.Add(fruitService.GetFruit());
-        fruits.Add(fruitService.GetFruit());
-
-        Assert.True(fruits.Count == 3);
-    }
 
     [Fact]
-    public async Task MovieServiceTest()
-    {
-        MovieService movieService = new();
-        List<Search> list = await movieService.GetMoviesAsync();
 
-        Assert.True(list.Count > 0);
-    }
 }
---
+++ Chat.Test/Tests.cs
@@ -0,0 +1,33 @@
+namespace Chat.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void FruitServiceTest()
+    {
+        FruitService fruitService = new();
+        List<Fruit> fruits = new();
+
+        // Add 3 fruits
+        fruits.Add(fruitService.GetFruit());
+        fruits.Add(fruitService.GetFruit());
+        fruits.Add(fruitService.GetFruit());
+
+        Assert.True(fruits.Count == 3);
+    }
+
+    [Test]
+    public async Task MovieServiceTest()
+    {
+        MovieService movieService = new();
+        List<Search> list = await movieService.GetMoviesAsync();
+
+        Assert.True(list.Count > 0);
+    }
+
+}
