8e192e8282364b9fc1cc6d22c6a0ae26f5f4127e
Ali <alifarahnak>
2022-06-05 21:03:37 +0000

    m

---
+++ Chat.UITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Chat.UITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chat.UITest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0f4c3d7-d42e-404f-906b-6bcee6cbfd55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Chat.UITest/UITest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Assert = NUnit.Framework.Assert;
+
+namespace Chat.UITest
+{
+    [TestFixture(Platform.Android)]
+    public class UITest
+    {
+        IApp app;
+        Platform platform;
+
+        public UITest(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+#if DEBUG
+            app.Repl();
+#endif
+            app.EnterText(ui => ui.Marked("username"), "admin");
+            app.EnterText(ui => ui.Marked("password"), "abcd1234");
+            app.Screenshot("LoginPage");
+            app.Tap(c => c.Marked("LoginButton"));
+            AppResult[] results = app.WaitForElement(c => c.Marked("LogoutButton"));
+            app.Screenshot("CollectionPage");
+            Assert.IsTrue(results.Any());
+        }
+    }
+
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                   .Android
+                   .InstalledApp("ms.chat.mobile")
+                   .Debug()
+                   .StartApp();
+            }
+
+                return ConfigureApp
+                    .iOS
+                    .InstalledApp("ms.chat.mobile")
+                    .StartApp();
+        }
+    }
+}
