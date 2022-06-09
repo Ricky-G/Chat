71e45dde2bc41e12efa811ae4d1d9a4d5231726a
Ali <alifarahnak>
2022-06-09 05:32:42 +0000

    m

--- Chat.Hub/Program.cs
+++ Chat.Hub/Program.cs
@@ -2,9 +2,14 @@ using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddSignalR(); 
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
     app.MapHub<Chat>(nameof(Chat));
+    app.MapGet("/", ()=> "Hello Index");
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.Run();
 
 public class Chat : Hub
