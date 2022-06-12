1ab63d6f8607682234783b6b3836f17c27587161
Ali <alifarahnak>
2022-06-12 20:43:01 +0000

    m

--- Chat.Core/ViewModel/LoginViewModel.cs
+++ Chat.Core/ViewModel/LoginViewModel.cs
@@ -18,9 +18,9 @@ public partial class LoginViewModel : BaseViewModel
     [ICommand]
     private async void Login(object s)
     {
-        await (s as Label).RotateTo(360, 500);
-        await (s as Label).RotateTo(0, 500);
-        await Task.Delay(100);
+       // await (s as Label).RotateTo(360, 500);
+       // await (s as Label).RotateTo(0, 500);
+     //   await Task.Delay(100);
         Preferences.Set("Exception", "");
         Exception = "";
         await Shell.Current.GoToAsync("//Tabs/Home", true);
