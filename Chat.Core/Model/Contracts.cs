a3567e4fdb4d2aaac220f9cb7cfbd4a556dd42a2
Ali <alifarahnak>
2022-06-15 07:52:01 +0200

    m

--- Chat.Core/Model/Contracts.cs
+++ Chat.Core/Model/Contracts.cs
@@ -4,5 +4,5 @@ namespace Chat.Core.Model;
 public record Search(string Title, string Year, string ImdbID, string Type, string Poster);
 public record MovieSearch(List<Search> Search, string TotalResults, string Response);
 public record Fruit(string Source, string Name);
-public record Message(string Name, string Body);
+public record struct Message(string Name, string Body);
 public record struct TelemetrySettings(string AppInsights, string QuickPulse);
