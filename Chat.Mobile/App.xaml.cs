276e478a8f5500f4dae547f26f4e6ba6b7d3f4c0
Ali <alifarahnak>
2022-07-24 19:59:36 +0200

    m

--- Chat.Mobile/App.xaml.cs
+++ Chat.Mobile/App.xaml.cs
@@ -9,4 +9,16 @@ public partial class App : Application
         MainPage = new Root();
         Shell.Current.GoToAsync("//Login");
     }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+
+       // App.Current.PageAppearing += Current_PageAppearing;
+    }
+
+    private void Current_PageAppearing(object sender, Page e)
+    {
+        Console.WriteLine($"Page Appearing: {e.GetType()}");
+    }
 }
