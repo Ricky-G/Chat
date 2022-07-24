d4bad36391aa65051fbb8b8daf92921970828ba5
Ali <alifarahnak>
2022-07-24 21:22:54 +0200

    m

--- Chat.Blazor/Program.cs
+++ Chat.Blazor/Program.cs
@@ -1,6 +1,8 @@
 using Chat.Blazor;
 using Chat.Core.ViewModel;
 using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -11,6 +13,15 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+var aiOptions = new ApplicationInsightsServiceOptions();
+// Disables adaptive sampling.
+aiOptions.EnableAdaptiveSampling = false;
+// Disables QuickPulse (Live Metrics stream).
+aiOptions.EnableQuickPulseMetricStream = true;
+aiOptions.ConnectionString = "InstrumentationKey=e14a93ed-fdf0-45db-8aec-5b20a4fd0fad;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/";
+builder.Services.AddApplicationInsightsTelemetry(aiOptions);
+builder.Services.ConfigureTelemetryModule<QuickPulseTelemetryModule>((module, o) => module.AuthenticationApiKey = "8hxec7lfgznbtb2jgeotb4hnrkbf087jnmwpejgl");
 
 //builder.Services.AddSingleton(Chat.Core.Globals.GetTelemetryClient());
 builder.Services.AddSingleton<HomeViewModel>();
--- Chat.Hub/Program.cs
+++ Chat.Hub/Program.cs
@@ -2,6 +2,7 @@
 using ChatCore;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
 using Microsoft.AspNetCore.SignalR;
 
 
@@ -18,8 +19,8 @@ aiOptions.EnableAdaptiveSampling = false;
 // Disables QuickPulse (Live Metrics stream).
 aiOptions.EnableQuickPulseMetricStream = true;
 aiOptions.ConnectionString = "InstrumentationKey=e14a93ed-fdf0-45db-8aec-5b20a4fd0fad;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/";
-
 builder.Services.AddApplicationInsightsTelemetry(aiOptions);
+builder.Services.ConfigureTelemetryModule<QuickPulseTelemetryModule>((module, o) => module.AuthenticationApiKey = "8hxec7lfgznbtb2jgeotb4hnrkbf087jnmwpejgl");
 
 var app = builder.Build();
 app.MapHub<Chat>(nameof(Chat));
