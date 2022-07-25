a71712d5f1fb1c2ef0d2bc686159bb2065ea0a5e
Ali <alifarahnak>
2022-07-25 19:22:51 +0200

    m

--- Chat.Blazor/Program.cs
+++ Chat.Blazor/Program.cs
@@ -14,7 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
-builder.Services.AddSingleton(Globals.GetTelemetryClient());
+//builder.Services.AddSingleton(Globals.GetTelemetryClient());
 builder.Services.AddSingleton<HomeViewModel>();
 
 await builder.Build().RunAsync();
