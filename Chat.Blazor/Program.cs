b6db85c52132a844285cc9b3463a8e69f32db227
Ali <alifarahnak>
2022-07-25 11:22:00 +0200

    m

--- Chat.Blazor/Program.cs
+++ Chat.Blazor/Program.cs
@@ -14,7 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-
+/*
 var aiOptions = new ApplicationInsightsServiceOptions();
 // Disables adaptive sampling.
 aiOptions.EnableAdaptiveSampling = false;
@@ -23,7 +23,7 @@ aiOptions.EnableQuickPulseMetricStream = true;
 aiOptions.ConnectionString = "InstrumentationKey=e14a93ed-fdf0-45db-8aec-5b20a4fd0fad;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/";
 builder.Services.AddApplicationInsightsTelemetry(aiOptions);
 builder.Services.ConfigureTelemetryModule<QuickPulseTelemetryModule>((module, o) => module.AuthenticationApiKey = "8hxec7lfgznbtb2jgeotb4hnrkbf087jnmwpejgl");
-
+*/
 builder.Services.AddSingleton(Globals.GetTelemetryClient());
 builder.Services.AddSingleton<HomeViewModel>();
 
--- Chat.Hub/Program.cs
+++ Chat.Hub/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton(Globals.GetTelemetryClient());
 
+/*
 
 var aiOptions = new ApplicationInsightsServiceOptions();
 // Disables adaptive sampling.
@@ -21,6 +22,7 @@ aiOptions.EnableQuickPulseMetricStream = true;
 aiOptions.ConnectionString = "InstrumentationKey=e14a93ed-fdf0-45db-8aec-5b20a4fd0fad;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/";
 builder.Services.AddApplicationInsightsTelemetry(aiOptions);
 builder.Services.ConfigureTelemetryModule<QuickPulseTelemetryModule>((module, o) => module.AuthenticationApiKey = "8hxec7lfgznbtb2jgeotb4hnrkbf087jnmwpejgl");
+*/
 
 var app = builder.Build();
 app.MapHub<Chat>(nameof(Chat));
